67c37f74a1f81a68bc59da066ea1fcf65db8286f
ext.agnieszkal <agnieszka.liszka@fitech.pl>
2021-01-05 11:50:03 +0100

    Add background color to register page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,55 +1,61 @@
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card">
-          <div class="card-header">Login</div>
-          <div class="card-body">
-            <div v-if="error" class="alert alert-danger">{{error}}</div>
-            <form action="#" @submit.prevent="submit">
-              <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
+  <v-parallax
+    height="665"
+    dark
+    src="https://bloximages.chicago2.vip.townnews.com/postregister.com/content/tncms/assets/v3/editorial/8/58/85810653-a184-5a1b-939f-c3e17e47e0ff/5bef4e5449b9b.image.jpg?resize=1200%2C800"
+  >
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="card">
+            <div class="card-header">Login</div>
+            <div class="card-body">
+              <div v-if="error" class="alert alert-danger">{{error}}</div>
+              <form action="#" @submit.prevent="submit">
+                <div class="form-group row">
+                  <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
 
-                <div class="col-md-6">
-                  <input
-                    id="email"
-                    type="email"
-                    class="form-control"
-                    name="email"
-                    value
-                    required
-                    autofocus
-                    v-model="form.email"
-                  />
+                  <div class="col-md-6">
+                    <input
+                      id="email"
+                      type="email"
+                      class="form-control"
+                      name="email"
+                      value
+                      required
+                      autofocus
+                      v-model="form.email"
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div class="form-group row">
-                <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                <div class="form-group row">
+                  <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
 
-                <div class="col-md-6">
-                  <input
-                    id="password"
-                    type="password"
-                    class="form-control"
-                    name="password"
-                    required
-                    v-model="form.password"
-                  />
+                  <div class="col-md-6">
+                    <input
+                      id="password"
+                      type="password"
+                      class="form-control"
+                      name="password"
+                      required
+                      v-model="form.password"
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div class="form-group row mb-0">
-                <div class="col-md-8 offset-md-4">
-                  <button type="submit" class="btn btn-primary">Login</button>
+                <div class="form-group row mb-0">
+                  <div class="col-md-8 offset-md-4">
+                    <button type="submit" class="btn btn-primary">Login</button>
+                  </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </v-parallax>
 </template>
 
 <script>
@@ -70,9 +76,7 @@ export default {
       firebase
         .auth()
         .signInWithEmailAndPassword(this.form.email, this.form.password)
-        .then(data => {
-          this.$router.replace({ name: "pickADish" });
-        })
+        .then(this.$router.replace({ name: "pickADish" }))
         .catch(err => {
           this.error = err.message;
         });
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,72 +1,78 @@
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card">
-          <div class="card-header">Register</div>
-          <div class="card-body">
-            <div v-if="error" class="alert alert-danger">{{error}}</div>
-            <form action="#" @submit.prevent="submit">
-              <div class="form-group row">
-                <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
+  <v-parallax
+    height="665"
+    dark
+    src="https://bloximages.chicago2.vip.townnews.com/postregister.com/content/tncms/assets/v3/editorial/8/58/85810653-a184-5a1b-939f-c3e17e47e0ff/5bef4e5449b9b.image.jpg?resize=1200%2C800"
+  >
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="card">
+            <div class="card-header">Register</div>
+            <div class="card-body">
+              <div v-if="error" class="alert alert-danger">{{error}}</div>
+              <form action="#" @submit.prevent="submit">
+                <div class="form-group row">
+                  <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
 
-                <div class="col-md-6">
-                  <input
-                    id="name"
-                    type="name"
-                    class="form-control"
-                    name="name"
-                    value
-                    required
-                    autofocus
-                    v-model="form.name"
-                  />
+                  <div class="col-md-6">
+                    <input
+                      id="name"
+                      type="name"
+                      class="form-control"
+                      name="name"
+                      value
+                      required
+                      autofocus
+                      v-model="form.name"
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
+                <div class="form-group row">
+                  <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
 
-                <div class="col-md-6">
-                  <input
-                    id="email"
-                    type="email"
-                    class="form-control"
-                    name="email"
-                    value
-                    required
-                    autofocus
-                    v-model="form.email"
-                  />
+                  <div class="col-md-6">
+                    <input
+                      id="email"
+                      type="email"
+                      class="form-control"
+                      name="email"
+                      value
+                      required
+                      autofocus
+                      v-model="form.email"
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div class="form-group row">
-                <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                <div class="form-group row">
+                  <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
 
-                <div class="col-md-6">
-                  <input
-                    id="password"
-                    type="password"
-                    class="form-control"
-                    name="password"
-                    required
-                    v-model="form.password"
-                  />
+                  <div class="col-md-6">
+                    <input
+                      id="password"
+                      type="password"
+                      class="form-control"
+                      name="password"
+                      required
+                      v-model="form.password"
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div class="form-group row mb-0">
-                <div class="col-md-8 offset-md-4">
-                  <button type="submit" class="btn btn-primary">Register</button>
+                <div class="form-group row mb-0">
+                  <div class="col-md-8 offset-md-4">
+                    <button type="submit" class="btn btn-primary">Register</button>
+                  </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </v-parallax>
 </template>
 
 
@@ -74,7 +80,6 @@
 import firebase from "firebase";
 
 export default {
-
   data() {
     return {
       form: {
@@ -95,7 +100,9 @@ export default {
             .updateProfile({
               displayName: this.form.name
             })
-            .then(() => { this.$router.push('/login')});
+            .then(() => {
+              this.$router.push("/login");
+            });
         })
         .catch(err => {
           this.error = err.message;
